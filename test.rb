30480df4d41330c2b38407f7c39a7054f29c6ced
aikutto <aikutto@gmail.com>
2017-02-07 16:24:56 +0300

    Merge branch 'master' of /home/aykut/ruby101/kardesler-pansiyon with conflicts.

--- test.rb
+++ test.rb
@@ -1,16 +1,20 @@
 require "./lib/musteri"
 require "./lib/oda"
+
+require "date"
 # m = Musteri.new
- #m.giris_yap "Aykut YILDIRIM", "101"
+# m.giris_yap "Aykut YILDIRIM", "101"
 
 # m= Musteri.aktif_musteri_bul 1
 # m.cikis_yap
 
 #o=Oda.new
 #o.oda_degistir("105","0")
-    Musteri.aktif_musteriler.each {|i| puts "#{i[0]})Adı Soyadı :#{i[1]}\n Oda Numarası : #{i[2]}\n Giris Tarihi : #{i[3]}\n" }
-    #p Oda.oda_goster("0")
-    
-    
-    
-    puts"SElam"
+
+
+# p Musteri.aktif_musteri_bul 1
+
+giris = "2017-02-07 10:57:36 +0300"
+
+t = Time.now - DateTime.strptime(giris, '%Y-%m-%d %H:%M:%S %z').to_time
+p (t / 86400).ceil
